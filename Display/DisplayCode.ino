f6aa4e991222e141605e5845c9f5f65c69aa86a2
Ashraf Elshazly <60497722+AshrafElshazly@users.noreply.github.com>
2021-10-12 19:23:43 +0200

    Display-process( Code & Design)

---
+++ Display/DisplayCode.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+LiquidCrystal_I2C lcd(0x24,20,4);
+Adafruit_SSD1306 oled(128, 64, &Wire, -1);
+
+String lcdText  = "Welcome LCD";
+String oledText = "Welcome OLED";
+
+void setup() {
+ lcdDisplay(lcdText);
+ oledDisplay(oledText);
+}
+
+void loop() {
+}
+
+void lcdDisplay(String text)
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(1,0);
+  lcd.print(text);
+}
+void oledDisplay(String text)
+{
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
+  }
+  oled.clearDisplay();
+  oled.setTextSize(1);
+  oled.setTextColor(WHITE);
+  oled.setCursor(0, 10);
+  oled.println(text); 
+  oled.display();
+}
