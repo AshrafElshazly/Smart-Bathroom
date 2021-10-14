a53f5cf52ec7d9c0b2f294b5db83bd42166861f1
Ashraf Elshazly <60497722+AshrafElshazly@users.noreply.github.com>
2021-10-14 22:07:13 +0200

    Add Sim

---
+++ Tester/CodeTest/CodeTest.ino
@@ -0,0 +1,17 @@
+#include <SmartBath.h>
+
+//Objects
+LiquidCrystal_I2C lcd(0x24,16,2);
+Adafruit_SSD1306 oled(128, 64, &Wire, -1);
+Servo motor1;
+//SoftwareSerial sim(2, 3); //(RX,TX)
+
+void setup()
+{
+
+}
+ 
+void loop()
+{
+}
+ 
