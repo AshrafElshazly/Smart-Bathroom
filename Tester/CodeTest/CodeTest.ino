350f66c23a3db451ee8f1f589b853e8e2e721658
Ashraf Elshazly <60497722+AshrafElshazly@users.noreply.github.com>
2021-10-15 04:38:22 +0200

    test

--- Tester/CodeTest/CodeTest.ino
+++ Tester/CodeTest/CodeTest.ino
@@ -3,15 +3,25 @@
 //Objects
 LiquidCrystal_I2C lcd(0x24,16,2);
 Adafruit_SSD1306 oled(128, 64, &Wire, -1);
-Servo motor1;
-//SoftwareSerial sim(2, 3); //(RX,TX)
+Servo door;
+SoftwareSerial sim(2, 3); //(RX,TX)
 
 void setup()
 {
-
+  for()
 }
  
 void loop()
 {
+  
 }
- 
+
+void outputPinConfig(int pin)
+{
+  pinMode(pin,OUTPUT);
+}
+
+void inputPinConfig(int pin)
+{
+  pinMode(pin,INPUT);
+}
