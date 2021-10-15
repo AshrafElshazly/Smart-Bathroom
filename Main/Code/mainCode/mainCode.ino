66f54a502f58cadb06c85af65e966e58c86463bb
Ashraf Elshazly <60497722+AshrafElshazly@users.noreply.github.com>
2021-10-15 18:02:15 +0200

    new code

--- Main/Code/mainCode/mainCode.ino
+++ Main/Code/mainCode/mainCode.ino
@@ -5,31 +5,44 @@ Adafruit_SSD1306 oled(128, 64, &Wire, -1);
 Servo door;
 SoftwareSerial sim(TX0,RX0);
 
+boolean flag  = true;
+boolean lcdFlag   = false;
+boolean oldFlag   = false;
+boolean flameFlag = false;
+
 void setup() {
-  pinMode(inputPins,INPUT);
-  pinMode(outputPins,OUTPUT);
+  pinConfig("OUTPUT",outputPins);
+  pinConfig("INPUT",inputPins);
 }
 
 void loop() {
-  //Check Flam Sensor
+
+  //Start Check Flam Sensor
   if(digitalRead(FLAME) == 1){
     //Turn on extinguishing fire pump
-    digitalWrite(EXTINGUISHING_FIRES,HIGH);
+    digitalWrite(EXTINGUISHING_FIRES,LOW);
     //Turn on buzzer
     digitalWrite(BUZZER,HIGH);
     //Send an emergency message 
     sendSMS(sim,"There is a fire ");
+    lcdDisplay(lcd,"Fire inside");
     }
+   //End Check Flam Sensor
+   
   //Check PIR Sensor
   if(digitalRead(PIR) == 0){
       lcdDisplay(lcd,"Empty,Enter Coin");
+      delay(20);
       //Check coin Machine
-      if(digitalRead(COIN_MACHINE) == 1){
+      if(digitalRead(COIN_MACHINE) == 1 && flag == true){
         //Open door
         servoControlle(door,DOOR,"open");
+        delay(100);
         lcdDisplay(lcd,"Busy");
+        delay(20);
         //Turn on the interior light
         digitalWrite(INTERIOR_LIGHT,HIGH);
+        delay(20);
         if(ultraResult(TRIG3,ECHO3,20)){
           //Close door
           servoControlle(door,DOOR,"close");
@@ -40,10 +53,10 @@ void loop() {
           delay(5000);
           servoControlle(door,DOOR,"close");
           }
-      }else{
+      }else if (flag == false){
         lcdDisplay(lcd,"Enter Valid Coin");
         }
-  }else{
+    }else{
     lcdDisplay(lcd,"Busy");
-  }  
+  }
 }
