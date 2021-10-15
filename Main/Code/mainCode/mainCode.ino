0dc04179bd53b3f172e8392bbe98e47888181838
Ashraf Elshazly <60497722+AshrafElshazly@users.noreply.github.com>
2021-10-15 04:37:30 +0200

    New Code

--- Main/Code/mainCode/mainCode.ino
+++ Main/Code/mainCode/mainCode.ino
@@ -2,13 +2,48 @@
 
 LiquidCrystal_I2C lcd(0x24,16,2);
 Adafruit_SSD1306 oled(128, 64, &Wire, -1);
-Servo motor1;
-SoftwareSerial sim(2, 3); //(RX,TX)
+Servo door;
+SoftwareSerial sim(TX0,RX0);
 
 void setup() {
-  
+  pinMode(inputPins,INPUT);
+  pinMode(outputPins,OUTPUT);
 }
 
 void loop() {
-
+  //Check Flam Sensor
+  if(digitalRead(FLAME) == 1){
+    //Turn on extinguishing fire pump
+    digitalWrite(EXTINGUISHING_FIRES,HIGH);
+    //Turn on buzzer
+    digitalWrite(BUZZER,HIGH);
+    //Send an emergency message 
+    sendSMS(sim,"There is a fire ");
+    }
+  //Check PIR Sensor
+  if(digitalRead(PIR) == 0){
+      lcdDisplay(lcd,"Empty,Enter Coin");
+      //Check coin Machine
+      if(digitalRead(COIN_MACHINE) == 1){
+        //Open door
+        servoControlle(door,DOOR,"open");
+        lcdDisplay(lcd,"Busy");
+        //Turn on the interior light
+        digitalWrite(INTERIOR_LIGHT,HIGH);
+        if(ultraResult(TRIG3,ECHO3,20)){
+          //Close door
+          servoControlle(door,DOOR,"close");
+        }else{
+          delay(5000);
+          //Turn on buzzer
+          digitalWrite(BUZZER,HIGH);
+          delay(5000);
+          servoControlle(door,DOOR,"close");
+          }
+      }else{
+        lcdDisplay(lcd,"Enter Valid Coin");
+        }
+  }else{
+    lcdDisplay(lcd,"Busy");
+  }  
 }
